e6840e6cde15ddb2189c2e9f7b34f1f9907bf4c0
qubit88 <qubertec@gmail.com>
2019-11-06 23:31:06 +0200

    add creating store, adding thunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
+import searchReducer from "./store/reducers/searchReducer";
+import { routes } from "./routes";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  max-width: 1200px;
+`;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  searchReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppContainer>{routes}</AppContainer>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
